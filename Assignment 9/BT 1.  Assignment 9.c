38c4c360b9b518399bc67766271ad9eb9c289ddd
Nguyễn Minh Tiến <58099088+ngminhtien98@users.noreply.github.com>
2020-06-24 18:16:26 +0700

    Update BT 1.  Assignment 9.c

--- Assignment 9/BT 1.  Assignment 9.c
+++ Assignment 9/BT 1.  Assignment 9.c
@@ -6,24 +6,22 @@
 //  Copyright © 2020 Nguyễn Minh Tiến. All rights reserved.
 // 1. Nhập vào 1 chuỗi, viết hàm tìm ký tự trong chuỗi
 
+#include <stdio.h>
+#include <string.h>
 
-//#include <stdio.h>
-//#include <string.h>
-//
-//int main() {
-//    char s[20];
-//    char c;
-//    printf("Hãy nhập chuỗi: ");
-//    scanf("%s", s);
-//    printf("Hãy nhập ký tự bạn muốn tìm: ");
-//    scanf("%c",&c);
-//    
-//    printf("%c", c);
-//    
-//    if(strchr(s,c) != NULL)
-//        printf("Có ký tự %c trong chuỗi vừa nhập.\n",c);
-//    else
-//        printf("Không có ký tự %c trong chuỗi vừa nhập.\n",c);
-//
-//    return 0;
-//}
+int main() {
+    char str[20];
+    char chr;
+    printf("Hãy nhập chuỗi: ");
+    scanf("%s", str);
+    getchar();
+    printf("Hãy nhập ký tự bạn muốn tìm: ");
+    scanf("%c",&chr);
+
+    if(strchr(str,chr) != NULL)
+        printf("Có ký tự %c trong chuỗi vừa nhập.\n",chr);
+    else
+        printf("Không có ký tự %c trong chuỗi vừa nhập.\n",chr);
+
+    return 0;
+}
