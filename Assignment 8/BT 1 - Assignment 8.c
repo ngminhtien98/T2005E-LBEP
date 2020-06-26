76b9dde20032204c1a17c958fe11d4967022dd4a
Nguyễn Minh Tiến <58099088+ngminhtien98@users.noreply.github.com>
2020-06-26 19:32:37 +0700

    Update

--- Assignment 8/BT 1 - Assignment 8.c
+++ Assignment 8/BT 1 - Assignment 8.c
@@ -6,35 +6,35 @@
 //  Copyright © 2020 Nguyễn Minh Tiến. All rights reserved.
 // 1. Nhập mảng n số nguyên, tìm số lượng các số dương liên tiếp nhiều nhất
 
-//#include <stdio.h>
-//#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 //
-////
-//int main() {
-//
-//    int n, *ary;
-//    printf("Hãy nhập số phần tử bạn muốn nhập: ");
-//    scanf("%d", &n);
-//    ary = (int *)malloc(n*sizeof(int));
-//    while(n<=0){
-//        printf("Số phần tử phải là một số nguyên dương, xin mời nhập lại: ");
-//        scanf("%d", &n);
-//    }
-//    for (int i = 0; i<n; i++) {
-//        printf("Nhập phần tử: ");
-//        scanf("%d", ary+i);
-//    }
-//
-//    int count = 0, x = 0;
-//    for(int i=0; i<n; i++){
-//        if(ary+i>0){
-//            count++;
-//            if(count > x)
-//                x = count;
-//        } else {
-//            count = 0;
-//        }
-//    }
-//    printf("Chuỗi các số nguyên dương nhiều nhất trong mảng là %d.\n", x);
-//    return(0);
-//}
+int main() {
+
+    int n, *ary;
+    printf("Hãy nhập số phần tử bạn muốn nhập: ");
+    scanf("%d", &n);
+    ary = (int *)malloc(n*sizeof(int));
+    while(n<=0){
+        printf("Số phần tử phải là một số nguyên dương, xin mời nhập lại: ");
+        scanf("%d", &n);
+    }
+    for (int i = 0; i<n; i++) {
+        printf("Nhập phần tử: ");
+        scanf("%d", ary+i);
+    }
+
+    int count = 0, x = 0;
+    for(int i=0; i<n; i++){
+        if(ary+i>0){
+            count++;
+            if(count > x)
+                x = count;
+        } else {
+            count = 0;
+        }
+    }
+    printf("Chuỗi các số nguyên dương nhiều nhất trong mảng là %d.\n", x);
+    return(0);
+}
